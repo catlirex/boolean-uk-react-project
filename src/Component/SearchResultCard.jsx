41a405fcc8651d07db6cf6f930bff2397aa2dff0
Catherine Li <catherine.uk.20@gmail.com>
2021-07-06 12:08:59 +0100

    FromToMap done, add logo assets

---
+++ src/Component/SearchResultCard.jsx
@@ -0,0 +1,8 @@
+export default function () {
+  <ResultCard>
+    <p>departure_time": "14:57"</p>
+    <p>"arrival_time": "15:41"</p>
+    <p>"duration": "00:44:00"</p>
+    <p>{`foot > tube:Jubilee > tube:Metropolitan> foot`}</p>
+  </ResultCard>;
+}
